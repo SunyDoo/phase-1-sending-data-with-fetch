7e6e50ec17816b1e0203894e1913c560d062e99f
Sunil <snilavarath@gmail.com>
2021-12-13 14:17:54 -0500

    commit tree 1f6a00e7c54acc89fdcb67aef81020bc9c738b3a

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(input1, input2){
+    let obj = {
+        name: input1,
+        email: input2
+    }
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body:JSON.stringify(obj)
+      }
+    return fetch ('http://localhost:3000/users',configurationObject)
+    .then(res => res.json())
+    .then (data => document.body.innerHTML = data.id)
+    .catch(function (error) {
+        alert('Unauthorized Access');
+        document.body.innerHTML =(error.message)
+      })
+}
+ 
